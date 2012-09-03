07d45847c2d8fa3a6423b749318dbc9bbf8c0f35
Mark Norman Francis <norm@cackhanded.net>
2012-09-03 12:02:24 +0100

    Produce error output in files when filtering URLs.

--- filter_csv.pl
+++ filter_csv.pl
@@ -1,34 +1,42 @@
 #!/usr/bin/env perl
 
 use strict;
+use warnings;
 use Text::CSV;
-use Data::Dumper;
 
 my $csv = Text::CSV->new({ binary => 1 });
 
 my $fh = *STDIN;
 my $titles = $csv->getline($fh);
 $csv->column_names(@$titles);
-my $line = 1;
+
+open my $formatting_errors,          '>', 'dist/formatting_errors.txt';
+open my $no_destination_errors,      '>', 'dist/no_destination_errors.txt';
+open my $looks_like_redirect_errors, '>', 'dist/looks_like_redirect_errors.txt';
 
 
 while (my $row = $csv->getline_hr($fh)) {
-    $line++;
     
     my $old    = $row->{'Old Url'};
     my $new    = $row->{'New Url'};
     my $status = $row->{'Status'};
     
     if ( ! length $old ) {
-        print STDERR "Entry without URL at line ${line}\n";
+        print $formatting_errors "Entry without URL: '${old},${new},${status}'\n";
+        next;
+    }
+    if ( ! length $status ) {
+        print $formatting_errors "Entry without status: '${old},${new},${status}'\n";
         next;
     }
     
-    if ( 301 == $status ) {
-        if ( ! length $new ) {
-            print STDERR "${old} has no redirect destination at line ${line}\n";
-            next;
-        }
+    if ( 301 == $status && ! length $new ) {
+        print $no_destination_errors "Redirect without destination: '${old},${new},${status}'\n";
+        next;
+    }
+    if ( 410 == $status && length $new ) {
+        print $looks_like_redirect_errors "Gone but with destination: '${old},${new},${status}'\n";
+        next;
     }
     
     print "${old},${new},${status}\n";
