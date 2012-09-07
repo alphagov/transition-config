a4238f954b0ab796bced684340089b7744938986
Mark Norman Francis <norm@cackhanded.net>
2012-09-07 01:27:36 +0100

    Tweak deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ export DEPLOY_TO=preview
 
 SERVER="${DEPLOY_TO}-redirector"
 
+SSH_CONFIG="-F ../alphagov-deployment/ssh_config"
+
 # redirector site configurations
 # scp -F ../ssh_config ./nginx_configs/*.conf "$SERVER":/etc/nginx/sites-available
-scp -F ../ssh_config ./dist/www.*.conf "$SERVER":/var/apps/redirector
+scp $SSH_CONFIG ./dist/www.*.conf "$USER@$SERVER":/var/apps/redirector
 
-ssh -F ../ssh_config "$SERVER" sudo /etc/init.d/nginx reload
+ssh $SSH_CONFIG "$SERVER" sudo /etc/init.d/nginx reload
