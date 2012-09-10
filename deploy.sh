fa9d07dd29aacfdd9b310eb3e6daf26b606184f8
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-09-10 17:11:14 +0100

    Adding a line to deploy.sh to also copy the redirections file to the appropriate directory - this may change

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,6 @@ SSH_CONFIG="-F ../alphagov-deployment/ssh_config"
 # redirector site configurations
 # scp -F ../ssh_config ./nginx_configs/*.conf "$SERVER":/etc/nginx/sites-available
 scp $SSH_CONFIG ./dist/www.*.conf "$USER@$SERVER":/var/apps/redirector
+scp $SSH_CONFIG ./nginx_configs/redirections "$USER@$SERVER":/var/apps/redirector
 
 ssh $SSH_CONFIG "$SERVER" sudo /etc/init.d/nginx reload
