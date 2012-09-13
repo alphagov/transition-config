ee1cb79ea39732abcf65cd0a8011daf94b919f13
Mark Norman Francis <mark.norman.francis@digital.cabinet-office.gov.uk>
2012-09-13 07:21:34 -0700

    Separate out nginx config into per-site configs.

--- jenkins.sh
+++ jenkins.sh
@@ -5,8 +5,6 @@ mkdir -p dist
 # run migratorator_mappings tests
 prove -l tests/migratorator_mappings/*.t
 
-# FIXME - MIGRATORATOR_AUTH
-
 # delete any previous mapping errors files
 rm -f dist/*
 
@@ -20,6 +18,7 @@ perl -Ilib create_mappings.pl dist/directgov_mappings_source.csv
 curl "https://docs.google.com/spreadsheet/pub?key=0AprXhKI73WmwdHMwaW1aZVphOUJ1a3dTTGhJSFV5dGc&single=true&gid=0&output=csv" > dist/businesslink_mappings_source.csv
 perl -Ilib create_mappings.pl dist/businesslink_mappings_source.csv
 
-# create redirections file
 
+# NGINX
+cp nginx_configs/*.conf dist
 exit
