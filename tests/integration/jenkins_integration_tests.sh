f6398123f221d0e195853f96aa35cb2f12411976
Mark Norman Francis <norm@cackhanded.net>
2012-09-27 11:39:16 +0100

    Try connecting to the deploy user.

--- tests/integration/jenkins_integration_tests.sh
+++ tests/integration/jenkins_integration_tests.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -e
 
+REDIRECTOR='deploy@redirector.production.alphagov.co.uk'
+
 # copy current workspace to redirector, to run the tests there
 # (much faster because almost zero network latency)
-ssh redirector.production.alphagov.co.uk mkdir -p /tmp/redirector
-rsync -av . redirector.production.alphagov.co.uk:/tmp/redirector/.
+ssh ${REDIRECTOR} mkdir -p /tmp/redirector
+rsync -av . ${REDIRECTOR}:/tmp/redirector/.
 
 
 # run tests on redirector
-ssh redirector.production.alphagov.co.uk \
+ssh ${REDIRECTOR} \
         'cd /tmp/redirector && sh run_integration_tests.sh'
 
 # copy back any artifacts created
-rsync -av redirector.production.alphagov.co.uk:/tmp/redirector/. .
+rsync -av ${REDIRECTOR}:/tmp/redirector/. .
