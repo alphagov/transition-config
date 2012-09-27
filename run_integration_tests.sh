be171dc41e5dac5b175321bb27e911821af16c2e
Mark Norman Francis <norm@cackhanded.net>
2012-09-27 17:41:33 +0100

    Graph the results of redirects on production.

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -12,3 +12,7 @@ perl tests_with_graphs.pl \
         --report-output dist/dashboard.html \
         --${DEPLOY_TO:-preview} \
         $graphs
+
+if [ "$DEPLOY_TO" = 'production' ]; then
+    perl tests/integration/graph_redirects_results.pl
+fi
---
+++ tests/integration/graph_redirects_results.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+use v5.10;
+use strict;
+use warnings;
+
+use Text::CSV;
+use Net::Statsd;
+
+
+
+my $base_namespace = 'govuk.app.redirector.ratified';
+
+graph_errors_in( 'dist/businesslink_redirects_test_output.csv', 'businesslink' );
+graph_errors_in( 'dist/directgov_redirects_test_output.csv',    'directgov'    );
+exit;
+
+
+sub graph_errors_in {
+    my $error_csv = shift;
+    my $namespace = shift;
+    
+    my $csv = Text::CSV->new({ binary => 1 }) 
+        or die "Cannot use CSV: " . Text::CSV->error_diag();
+    
+    open( my $fh, "<", $error_csv ) 
+        or die "${error_csv}: $!";
+    
+    my $names = $csv->getline( $fh );
+    $csv->column_names( @$names );
+    
+    my %statuses;
+    while ( my $row = $csv->getline_hr( $fh ) ) {
+        my $actual_status = $row->{'New Url Status'};
+        $statuses{$actual_status}++;
+    }
+    
+    foreach my $status_code ( keys %statuses ) {
+        next unless $status_code;
+        
+        my $graph_name = sprintf '%s.%s.redirects.returned_%s',
+                            $base_namespace,
+                            $namespace,
+                            $status_code;
+        
+        say "$graph_name = $statuses{$status_code}";
+        Net::Statsd::gauge( $graph_name, $statuses{$status_code} );
+    }
+}
