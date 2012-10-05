818df2c6ff4cf22c0009d840b23aabbd65006941
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-10-05 11:53:11 +0100

    How to deal with different domain names.
    Unimplemented, in comment, based on information from BT.

--- tools/process-logs.sh
+++ tools/process-logs.sh
@@ -27,8 +27,31 @@ do
 				 gzip -d -c "$filename" | awk '/^[^#]/ { print $5 " " $6 }' | sed -e 's+^/wwwt+http://www+' -e 's+businesslink.gov.uk.akadns.net/+businesslink.gov.uk/+'
 			;;
 			webserver*)
-				 # a mixture of devolved sites
-				 #gzip -d -c "$filename" | awk '/^[^#]/ { print $7 " " $9 }' | sed -e 's+^+http://www.businesslink.gov.uk+'
+				# a mixture of devolved sites
+				# https-prod http://www.businesslink.gov.uk / http://online.businesslink.gov.uk
+
+				# https-online https://online.businesslink.gov.uk
+
+				# https-ukwelcomes http://www.ukwelcomes.businesslink.gov.uk /
+				# http://online.ukwelcomes.businesslink.gov.uk
+
+				# https-online-ukwelcomes https://online.ukwelcomes.businesslink.gov.uk
+
+				# https-wales http://business.wales.gov.uk / https://business.wales.gov.uk
+
+				# https-bgateway http://www.business.scotland.gov.uk /
+				# https://www.business.scotland.gov.uk
+
+				# https-ini http://www.nibusinessinfo.co.uk / https://www.nibusinessinfo.co.uk
+
+				# https-301war This is used to add the routing onto received base
+				# domains. i.e. http://www.businesslink.gov.uk becomes
+				# http://www.businesslink.gov.uk/bdotg/action/home.
+
+			 	# e.g. GET /bdotg/action/openpopup?flag=e&itemId=1075331868&r.i=1081676012&r.t=RESOURCES&site=202&type=ONEOFFPAGE HTTP/1.1" 200 4184 "-" "Mozilla/5.0 (comp
+				# atible; AhrefsBot/3.1; +http://ahrefs.com/robot/)" GET "HTTP/1.1" https-bgateway
+
+				#gzip -d -c "$filename" | awk '/^[^#]/ { print $7 " " $9 }' | sed -e 's+^+http://www.businesslink.gov.uk+'
 			;;
 			prod_*) 
 				 gzip -d -c "$filename" | awk '/^[^#]/ { print $5 " " $6 }' | sed -e 's+^/theclubprod.download.akamai.com+http://www.direct.gov.uk+' -e 's+^/+http://+'
