205f4d0e1d717a4dac35e604237ce4ee2f024937
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-10-08 15:13:16 +0100

    Replacing comments with logging and removing the join that is not required (logically, those two files have gone down different paths so there won't be anything that is in one that is also in the other)

--- tools/prune.sh
+++ tools/prune.sh
@@ -6,26 +6,23 @@
 
 site=directgov
 
-# find the end point goal of all the redirects
+echo $(date +"%H:%M:%S") "curling each redirect to find where it redirects to..." >&2
 awk '$3 ~ /^3/ { print $1 }' "${site}.txt" | while read url; do curl -f -s -o /dev/null -w '%{url_effective} %{http_code} %{redirect_url}\n' "$url"; done > "${site}-redirects".txt
 
-# if it does not have a status code of 3xx, 4xx or 5xx we can assume it is not an error
+echo $(date +"%H:%M:%S") "moving those we can assume aren't errors (those without status of 3xx, 4xx, or 5xx) to ${site}-nonerrors.txt..." >&2
 awk '$3 !~ /^[3-5]/ { print }' < ${site}.txt | sort > ${site}-nonerrors.txt
 
-# if the status code is 3xx it may redirect to an error
+echo $(date +"%H:%M:%S") "sorting 3xxs into those that redirect to errors and those that do not..." >&2
 awk '$3 ~ /(sendHTTPError|404.html|500.html)/ { print }' < "${site}-redirects.txt" | sort > ${site}-errors.txt
 awk '$3 !~ /(sendHTTPError|404.html|500.html)/ { print }' < "${site}-redirects.txt" | sort > ${site}-valid-redirects.txt
 
-# print out everything in non-errors except that which is in errors.
-join -v 1 ${site}-nonerrors.txt ${site}-errors.txt > ${site}-good.txt
+echo $(date +"%H:%M:%S") "concatenate non-errors and valid-redirects..." >&2
+cat ${site}-valid-redirects.txt ${site}-nonerrors.txt | sort | uniq > ${site}-testable-urls.txt
 
-# now concatenate non-errors and valid-redirects
-cat ${site}-valid-redirects.txt ${site}-good.txt | sort | uniq > ${site}-testable-urls.txt
-
-# format for testing that day's output
+echo $(date +"%H:%M:%S") "formatting to csv for testing that day's output..." >&2
 sort -k2 -nr "${site}-testable-urls.txt" | awk 'BEGIN { print "Old Url,Count,Status" }
  { print "\"" $1 "\"," $2 "," $3 }' > ${site}-testable.csv
 
-# add the URLs only to the $site-all file
+echo $(date +"%H:%M:%S") "adding URLs only to site-all.txt..." >&2
 ( awk '{ print $1 }' "${site}-testable-urls.txt"; cat ../${site}-all.txt ) | sort | uniq > new_${site}_all.txt
 mv new_${site}_all.txt ../${site}-all.txt
