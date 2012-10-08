6206f2ab9cd827547ea886542d312549fd78ca5c
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-10-08 15:53:46 +0100

    We do not want to lose count when we test redirects.
    This change propagates count through to valid-redirects and also only passes URL and count through to the final testable file, as that's all we care about here.

--- tools/prune.sh
+++ tools/prune.sh
@@ -7,17 +7,17 @@
 site=directgov
 
 echo $(date +"%H:%M:%S") "curling each redirect to find where it redirects to..." >&2
-awk '$3 ~ /^3/ { print $1 }' "${site}.txt" | while read url; do curl -f -s -o /dev/null -w '%{url_effective} %{http_code} %{redirect_url}\n' "$url"; done > "${site}-redirects".txt
+awk '$3 ~ /^3/ { print $1 " " $2 }' "${site}.txt" | while read url count; do curl -f -s -o /dev/null -w '%{url_effective} %{http_code} %{redirect_url}' "$url"; echo " " $count; done > "${site}-redirects".txt
 
 echo $(date +"%H:%M:%S") "moving those we can assume aren't errors (those without status of 3xx, 4xx, or 5xx) to ${site}-nonerrors.txt..." >&2
 awk '$3 !~ /^[3-5]/ { print }' < ${site}.txt | sort > ${site}-nonerrors.txt
 
 echo $(date +"%H:%M:%S") "sorting 3xxs into those that redirect to errors and those that do not..." >&2
 awk '$3 ~ /(sendHTTPError|404.html|500.html)/ { print }' < "${site}-redirects.txt" | sort > ${site}-errors.txt
-awk '$3 !~ /(sendHTTPError|404.html|500.html)/ { print }' < "${site}-redirects.txt" | sort > ${site}-valid-redirects.txt
+awk '$3 !~ /(sendHTTPError|404.html|500.html)/ { print }' < "${site}-redirects.txt" | awk '{ print $1 " " $4 }' | sort > ${site}-valid-redirects.txt
 
 echo $(date +"%H:%M:%S") "concatenate non-errors and valid-redirects..." >&2
-cat ${site}-valid-redirects.txt ${site}-nonerrors.txt | sort | uniq > ${site}-testable-urls.txt
+cat ${site}-valid-redirects.txt ${site}-nonerrors.txt | awk '{ print $1 " " $2 }' | sort | uniq > ${site}-testable-urls.txt
 
 echo $(date +"%H:%M:%S") "formatting to csv for testing that day's output..." >&2
 sort -k2 -nr "${site}-testable-urls.txt" | awk 'BEGIN { print "Old Url,Count,Status" }
