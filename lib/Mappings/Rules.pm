32ae5775512badebd5e532406ab946d4097301f9
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-10-15 14:32:55 +0100

    Revert "Change gone-welsh as 310 to Welsh page, not 410."
    This reverts commit e8e863ee0b1d411ba860ff1b6eba7b57f59958c9.
    
    Because the change should be to the mappings in the migratorator not through code.

--- lib/Mappings/Rules.pm
+++ lib/Mappings/Rules.pm
@@ -208,16 +208,10 @@ sub location_config {
     }
     elsif ( 'closed' eq $mapping_status ) {
         if ( '410' eq $self->{'status'} ) {
-            if ( defined $self->{'whole_tag'} && $self->{'whole_tag'} =~ m{gone-welsh} ) {
-                # 410 Gone Welsh (actually a 301 to the Why No Welsh page)
-                $config = "location ~* ^$self->{'old_url_relative'}\$ { return 301 https://www.gov.uk/cymraeg; }\n"
-            }
-            else {
-                # 410 Gone
-                $config = "location ~* ^$self->{'old_url_relative'}\$ { return 410; }\n";
-                $suggested_links_type = 'location_suggested_links';
-                $suggested_links = $self->get_suggested_link( $self->{'old_url_relative'} );
-            }
+            # 410 Gone
+            $config = "location ~* ^$self->{'old_url_relative'}\$ { return 410; }\n";
+            $suggested_links_type = 'location_suggested_links';
+            $suggested_links = $self->get_suggested_link( $self->{'old_url_relative'} );
         }
         elsif ( '301' eq $self->{'status'} ) {
             # 301 Moved Permanently
