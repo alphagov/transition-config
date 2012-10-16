6bbb3ed67b2bd15ecde1b79b2f5b64580d59eaa1
psd <psd@whatfettle.com>
2012-10-16 13:15:44 +0100

    broken page generation for tesing transactions

--- tools/txns.sh
+++ tools/txns.sh
@@ -3,8 +3,8 @@
 export IFS=,·
 while read id url
 do
-       echo "<h2>$id: <a href="$url"><\/a><\/h2>"
-       echo "<iframe src="$url" width="80%" height="600px"><\/iframe>/"
+       echo "<h2>$id: <a href="$url"></a></h2>"
+       echo "<iframe src="$url" width="80%" height="600px"></iframe>"
 
 done  < data/lrc_transactions_source.csv > txns.html
 
