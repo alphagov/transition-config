dcf23a88f01b92f47a95e77713c1e92062fd9a7c
Mark Norman Francis <norm@cackhanded.net>
2012-10-17 11:43:06 +0100

    Add viewport declaration to gone pages.

--- redirector/410_header.php
+++ redirector/410_header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>This page has been archived</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <!--[if lt IE 9]>
     <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
