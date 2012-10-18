41e3299f36a8310de0a13bb1f313bbfe34143c98
Mark Norman Francis <norm@cackhanded.net>
2012-10-18 14:42:38 +0100

    Suggested links on DG_XXX pages.

--- redirector/410_suggested_links.php
+++ redirector/410_suggested_links.php
@@ -9,6 +9,14 @@ if ( isset($matches[0]) && isset($query_suggested_links[$matches[0]]) ) {
     $suggested_links = $query_suggested_links[$matches[0]];
 }
 
+preg_match( "/dg_\d+/i", $_SERVER['REQUEST_URI'], $matches );
+if ( isset($matches[0]) ) {
+    $match = strtolower($matches[0]);
+    if ( isset( $location_suggested_links[$match] ) ) {
+        $suggested_links = $location_suggested_links[$match];
+    }
+}
+
 if ( isset($suggested_links) ) {
     echo "<p>For more information on this topic you may want to visit $suggested_links.</p>";
 }
