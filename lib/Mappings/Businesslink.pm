b0092c6f9d14fcf83875309234c567b9eec8e471
Mark Norman Francis <norm@cackhanded.net>
2012-10-23 16:48:35 +0100

    Give ukwelcomes.bl its own redirect map.

--- lib/Mappings/Businesslink.pm
+++ lib/Mappings/Businesslink.pm
@@ -55,8 +55,12 @@ sub actual_nginx_config {
         }
         
         # this is to deal with online, which has exactly the same rules
-        # as www - should be handled better        
-        $self->{'old_url_parts'}{'host'} = 'www.businesslink.gov.uk'; 
+        # as www - should be handled better
+        my $host = $self->{'old_url_parts'}{'host'};
+        
+        $self->{'old_url_parts'}{'host'} = 'www.businesslink.gov.uk'
+            if 'online.businesslink.gov.uk' eq $host
+                || 'businesslink.gov.uk' eq $host;
         
         return(
             $self->{'old_url_parts'}{'host'},
