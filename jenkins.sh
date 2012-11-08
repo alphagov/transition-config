1441f8edef394291193f926279cbbf997950e829
Mark Norman Francis <norm@cackhanded.net>
2012-11-08 11:52:48 +0000

    Add DCLG to the redirector.

--- jenkins.sh
+++ jenkins.sh
@@ -15,9 +15,11 @@ perl -Ilib create_mappings.pl dist/directgov_mappings_source.csv
 # BUSINESSLINK
 curl "https://docs.google.com/spreadsheet/pub?key=0AprXhKI73WmwdHMwaW1aZVphOUJ1a3dTTGhJSFV5dGc&single=true&gid=0&output=csv" > dist/businesslink_mappings_source.csv
 perl -Ilib create_mappings.pl dist/businesslink_mappings_source.csv
-
-# OTHER TEST DATA
 cp data/businesslink_piplink_redirects_source.csv dist
+
+# DCLG
+cp data/communities_mappings_source.csv dist
+perl -Ilib create_mappings.pl dist/communities_mappings_source.csv
 
 # NGINX
 rsync -a redirector/. dist/.
@@ -47,6 +49,12 @@ cat \
     redirector/static/dg/410.html \
         > dist/static/dg/410.php
 cp redirector/410_suggested_links.php dist/static/dg
+
+cat \
+    redirector/410_preamble.php \
+    redirector/410_header.php \
+    redirector/static/communities/410.html \
+        > dist/static/communities/410.php
 
 prove -l tests/unit/sources/valid_lines.t
 
