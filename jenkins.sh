249e48242e00a28b2aee7a36c2fbfa3c4e0b98a1
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-11-13 13:00:03 +0000

    Deleting curl from googledocs as this has now been tested.

--- jenkins.sh
+++ jenkins.sh
@@ -13,7 +13,6 @@ curl "https://${MIGRATORATOR_AUTH}@migratorator.production.alphagov.co.uk/mappin
 perl -Ilib create_mappings.pl dist/directgov_mappings_source.csv
 
 # BUSINESSLINK
-# curl "https://docs.google.com/spreadsheet/pub?key=0AprXhKI73WmwdHMwaW1aZVphOUJ1a3dTTGhJSFV5dGc&single=true&gid=0&output=csv" > dist/businesslink_mappings_source.csv
 cp data/businesslink_mappings_source.csv dist
 perl -Ilib create_mappings.pl dist/businesslink_mappings_source.csv
 cp data/businesslink_piplink_redirects_source.csv dist
