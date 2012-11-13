84ed8136d3239e9e212bb8ca29e3b1fc77142745
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-11-13 15:31:33 +0000

    Removed too much when deleting the parallelisation of tests.
    (Parallelisation makes the tests faster but causes some to incorrectly fail due to rate-limiting).

--- run_comprehensive_integration_tests.sh
+++ run_comprehensive_integration_tests.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 echo DEPLOY_TO=$DEPLOY_TO
-prove tests/integration/ratified/ tests/integration/sample/ tests/integration/config_rules/ tools/test-log.pl
+prove -lr tests/integration/ratified/ tests/integration/sample/ tests/integration/config_rules/ tools/test-log.pl
