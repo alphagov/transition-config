c42f2d0fb24eb2862d603c8589dde0fc28659909
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-12-11 15:17:34 +0000

    Adding regression tests to the integration tests - really this time.

--- run_subset_integration_tests.sh
+++ run_subset_integration_tests.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 echo DEPLOY_TO=$DEPLOY_TO
-prove -lj4 tests/integration/config_rules/ tests/integration/sample/ tools/test-log.pl
+prove -lj4 tests/integration/config_rules/ tests/integration/sample/ tests/integration/regressions/ tools/test-log.pl
