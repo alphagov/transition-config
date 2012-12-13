501e5ae6571435072907d52285e81cd0a6bf3e36
Mark Norman Francis <norm@cackhanded.net>
2012-12-13 15:31:23 +0000

    Improve subset integration test runner.
    - fixes 'regressions' becoming 'regression'
    - CONCURRENT_TESTS can be set to perform more (or less) at once
    - individual tests on separate lines to be more readable

--- run_subset_integration_tests.sh
+++ run_subset_integration_tests.sh
@@ -4,4 +4,9 @@ set -e
 set -x
 
 echo DEPLOY_TO=$DEPLOY_TO
-prove -lj4 tests/integration/config_rules/ tests/integration/sample/ tests/integration/regressions/ tools/test-log.pl
+
+prove -l${CONCURRENT_TESTS:-j4}         \
+    tests/integration/config_rules/     \
+    tests/integration/sample/           \
+    tests/integration/regression/       \
+    tools/test-log.pl
