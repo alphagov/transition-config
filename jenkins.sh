7f26f75f58d66066139f007b0e742d2be0c1156e
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-01-08 15:34:23 +0000

    Clarifying what is going on in Jenkins.sh
    and removing some unnecessary code. First pass.

--- jenkins.sh
+++ jenkins.sh
@@ -5,46 +5,30 @@ mkdir -p dist
 
 source sites.sh
 
-function error_exit() {
-    echo "$@" 1>&2
-    exit 1
-}
-
-# run unit logic tests
+echo "Running unit tests..."
 prove -lj4 tests/unit/logic/*.t
 
-# delete previous config, error files, etc
+echo "Creating fresh copies of data sources in dist directory"
 rm -rf dist/*
-
-# copy the mappings source files to dist
-TEST_FILES=''
 for site in ${REDIRECTOR_SITES[@]}; do
     cp data/mappings/${site}.csv dist/${site}_mappings_source.csv
-    
-    test_file="tests/unit/sources/${site}_valid_lines.t"
-    [ -f $test_file ] || error_exit "No test '${test_file}'"
-    TEST_FILES="${TEST_FILES} ${test_file}"
 done
-
-# Directgov mappings are finalised so no need to do this unless required
-# pending a discussion of how the migratorator will be used in future, commenting this out for possible later removal
-# curl "https://${MIGRATORATOR_AUTH}@migratorator.production.alphagov.co.uk/mappings.csv" > dist/directgov_mappings_source.csv
-
-# copy extra businesslink piplinks
 cp data/businesslink_piplink_redirects_source.csv dist
 
-# check the data sources for problems
-prove -lj4 $TEST_FILES tests/unit/sources/directgov_valid_lines.t
+echo "Testing sources are valid..."
+for site in ${REDIRECTOR_SITES[@]}; do
+    prove -l tests/unit/sources/${site}_valid_lines.t
+done
 
-# create the mappings from the source CSV files
+echo "Creating mappings from sources..."
 for site in ${REDIRECTOR_SITES[@]}; do
     perl -Ilib create_mappings.pl dist/${site}_mappings_source.csv
 done
 
-# copy configuration to dist for archiving
+echo "Copying configuration to dist directory..."
 rsync -a redirector/. dist/.
 
-# create the 410 pages
+echo "Creating 410 pages..."
 cat \
     redirector/410_preamble.php \
     dist/www.businesslink.gov.uk.*suggested_links*.conf \
@@ -89,7 +73,7 @@ for site in ${REDIRECTOR_SITES[@]}; do
             > dist/static/${site}/410.php
 done
 
-# generate sitemaps
+echo "Generating sitemaps..."
 perl tools/sitemap.pl dist/directgov_mappings_source.csv 'www.direct.gov.uk' > dist/static/dg/sitemap.xml
 prove bin/test_sitemap.pl :: dist/static/dg/sitemap.xml www.direct.gov.uk
 perl tools/sitemap.pl dist/businesslink_mappings_source.csv 'www.businesslink.gov.uk' 'online.businesslink.gov.uk' > dist/static/bl/sitemap.xml
