d02ee47dd847fef7a1860bbea8874a58e17d896e
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-01-17 17:13:47 +0000

    Double equals doesn't work on jenkins

--- run_in_progress_mappings.sh
+++ run_in_progress_mappings.sh
@@ -10,7 +10,7 @@ echo DEPLOY_TO=$DEPLOY_TO
     read titles
     while read site redirected rest
     do
-    	if [ $redirected == N ]; then
+    	if [ $redirected = N ]; then
             prove -l tests/unit/sources/${site}_valid_lines.t
 			prove -l tests/in_progress/${site}/
         fi
