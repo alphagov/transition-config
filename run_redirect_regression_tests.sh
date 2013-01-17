503c7ce0cfc7066e578c52d7cd116c0dee2de4cf
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-01-17 15:22:13 +0000

    Update regression test to use sites.csv

--- run_redirect_regression_tests.sh
+++ run_redirect_regression_tests.sh
@@ -2,18 +2,22 @@
 
 set -x
 
-source sites.sh
-
 echo DEPLOY_TO=$DEPLOY_TO
-
 
 prove -l tests/integration/config_rules/     \
     	tests/integration/regression/   	 \
+	    tests/integration/sample/           \
 		tools/test-log.pl					 \
 		tests/regression/businesslink_piplinks.t
 
-
-for site in ${REDIRECTED_SITES[@]}; do
-	prove -l tests/unit/sources/${site}_valid_lines.t
-	prove -l tests/regression/${site}.t
-done
+(
+	IFS=,
+    read titles
+    while read site redirected rest
+    do
+    	if [ $redirected == Y ]; then
+            prove -l tests/unit/sources/${site}_valid_lines.t
+			prove -l tests/regression/${site}.t
+        fi
+    done
+) < sites.csv
