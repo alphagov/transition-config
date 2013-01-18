8e96f7f6e5c0b8fd54088209a5e3c1d8dfe889a1
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-01-18 15:08:35 +0000

    Fix script errors; force download of mappings

--- tools/generate-redirects.sh
+++ tools/generate-redirects.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ "$#" = "0" ]; then
-    echo "Usage: $0 department-name"
+    echo "Usage: $0 department-name http_user http_pass"
     exit 1
+fi
+
+if [ ! -f ./document_mappings.csv ]; then
+    wget https://$2:$3@whitehall-admin.production.alphagov.co.uk/government/document_mappings.csv
 fi
 
 department=$1
@@ -20,6 +24,5 @@ set -e
 $make_mappings_file
 ./tools/fold-mappings.rb < $mappings_out > $folded_mappings
 mv $folded_mappings $mappings_out
-set +e
 
 echo "Done"
