6f26722035c1d2b0d6687dddf7bd096df572847e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-01-18 13:40:10 +0000

    Apply various spreadsheets in order of importance

--- tools/fetch_decc_mappings.rb
+++ tools/fetch_decc_mappings.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby -w
 
 base_dir = File.expand_path("..", File.dirname(__FILE__))
-missing_mappings_url = "https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=0&output=csv"
 
 require_relative "mapping_fetcher"
 fetcher = MappingFetcher.new("decc")
 
 # Note if there are duplicates then the FIRST mapping is used, so order matters here
-fetcher.add_source(MappingFetcher::RemoteCsvSource.new(missing_mappings_url))
+{
+  harvester: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=3&output=csv',
+  furls: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=6&output=csv',
+  manual: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=0&output=csv',
+  analytics: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=4&output=csv',
+  sitemap: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=8&output=csv',
+  other: 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdDF4SGR6TTBVd0Q1M2dlbmxZTWpSeFE&single=true&gid=10&output=csv'
+}.each do |_, url|
+  fetcher.add_source(MappingFetcher::RemoteCsvSource.new(url))
+end
 
 # download this from https://whitehall-admin.production.alphagov.co.uk/government/document_mappings.csv
 fetcher.remap_new_urls_using(base_dir + "/document_mappings.csv")
