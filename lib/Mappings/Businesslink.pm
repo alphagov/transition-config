81431cd19201923a710d63b920b2951e8df3de1d
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-01-24 14:10:05 +0000

    No longer required to be in an if block
    The Businesslink config only generated a map key if there was a query string, and if there wasn't a query string, called back to the generic location config. There is now no need for this if block as we are only calling the Businesslink config generator if there is a query string.

--- lib/Mappings/Businesslink.pm
+++ lib/Mappings/Businesslink.pm
@@ -15,61 +15,55 @@ sub actual_nginx_config {
     my $suggested_links;
     my $archive_link;
     my $mapping_status = '';
-    
-    if ( defined $self->{'old_url_parts'}{'query'} ) {
-        $mapping_status = lc $self->{'whole_tag'}
-            if defined $self->{'whole_tag'};
-        
-        my $map_key = $self->get_map_key( $self->{'old_url_parts'} );
-        if ( defined $map_key ) {
-            if ( defined $self->{'duplicates'}{$map_key} ) {
-                $config_or_error_type = 'duplicate_entry_error';
-                $config_line = $self->{'old_url'} . "\n";
-            }
-            elsif ( '410' eq $self->{'status'} ) {
-                # 410 Gone
-                $config_or_error_type = 'gone_map';
-                $config_line = "~${map_key} 410;\n";
-                $suggested_links_type = 'suggested_links_map';
-                $suggested_links = $self->get_suggested_link($map_key, 1);
-                $archive_link = $self->get_archive_link($map_key);
-            }
-            elsif ( '301' eq $self->{'status'} ) {
-                if ( length $self->{'new_url'}) {
-                    # 301 Moved Permanently
-                    $config_or_error_type   = 'redirect_map';
-                    $config_line = "~${map_key} $self->{'new_url'};\n";
-                } 
-                else {
-                    $config_or_error_type = 'no_destination_error';
-                    $config_line = "$self->{'old_url'}\n";
-                }
-            }
-            elsif ( '302' eq $self->{'status'} ) {
-                $config_or_error_type   = 'awaiting_content_map';
-                $config_line = "~${map_key} https://www.gov.uk/browse/business/maritime;\n";
-            }
-            $self->{'duplicates'}{$map_key} = 1;
+
+    $mapping_status = lc $self->{'whole_tag'}
+        if defined $self->{'whole_tag'};
+
+    my $map_key = $self->get_map_key( $self->{'old_url_parts'} );
+    if ( defined $map_key ) {
+        if ( defined $self->{'duplicates'}{$map_key} ) {
+            $config_or_error_type = 'duplicate_entry_error';
+            $config_line = $self->{'old_url'} . "\n";
         }
-        else {
-            $config_or_error_type = 'no_map_key_error'; 
-            $config_line = "$self->{'old_url'}\n"; 
+        elsif ( '410' eq $self->{'status'} ) {
+            # 410 Gone
+            $config_or_error_type = 'gone_map';
+            $config_line = "~${map_key} 410;\n";
+            $suggested_links_type = 'suggested_links_map';
+            $suggested_links = $self->get_suggested_link($map_key, 1);
+            $archive_link = $self->get_archive_link($map_key);
         }
-        
-        return(
-            $self->{'old_url_parts'}{'host'},
-            $config_or_error_type,
-            $config_line,
-            $suggested_links_type,
-            $suggested_links,
-            $archive_link
-        );
+        elsif ( '301' eq $self->{'status'} ) {
+            if ( length $self->{'new_url'}) {
+                # 301 Moved Permanently
+                $config_or_error_type   = 'redirect_map';
+                $config_line = "~${map_key} $self->{'new_url'};\n";
+            }
+            else {
+                $config_or_error_type = 'no_destination_error';
+                $config_line = "$self->{'old_url'}\n";
+            }
+        }
+        elsif ( '302' eq $self->{'status'} ) {
+            $config_or_error_type   = 'awaiting_content_map';
+            $config_line = "~${map_key} https://www.gov.uk/browse/business/maritime;\n";
+        }
+        $self->{'duplicates'}{$map_key} = 1;
     }
-    # if no query string, we treat it as a furl
     else {
-        return $self->location_config();
-    }    
-    
+        $config_or_error_type = 'no_map_key_error'; 
+        $config_line = "$self->{'old_url'}\n";
+    }
+
+    return(
+        $self->{'old_url_parts'}{'host'},
+        $config_or_error_type,
+        $config_line,
+        $suggested_links_type,
+        $suggested_links,
+        $archive_link
+    );
+
 }
 sub get_map_key {
     my $self         = shift;
