22c0da2ced7cda1e01a0e39d92aa14cf0dc9f655
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-01-29 09:45:13 +0000

    Only extract mappings that can be force published

--- tools/extract-mappings.rb
+++ tools/extract-mappings.rb
@@ -7,12 +7,13 @@ abort "Usage: #{__FILE__} http://example.com < document_mappings.csv" unless ARG
 input = CSV.parse($stdin, headers: true).select { |row|
   /#{Regexp.escape(ARGV.first)}/ =~ row['Old Url']
 }.collect { |row|
-  [row['Old Url'], row['New Url']]
+  [row['Old Url'], row['New Url']] if ['published','draft','submitted'].include?(row['State'])
 }
 
 out = CSV.generate do |csv|
   csv << ["Old Url", "New Url"]
   input.each do |row|
+    next if row.nil?
     csv << row
   end
 end
