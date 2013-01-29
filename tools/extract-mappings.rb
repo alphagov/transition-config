03de6d6aeed4cccc9957ef884a8bfa42714d899a
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-01-29 11:51:35 +0000

    Hack to work around broken attachment CSV rows
    Document dump CSV has 7 columns, attachments only appear to have 4.

--- tools/extract-mappings.rb
+++ tools/extract-mappings.rb
@@ -4,10 +4,15 @@ require 'csv'
 
 abort "Usage: #{__FILE__} http://example.com < document_mappings.csv" unless ARGV.size > 0
 
+def include_row?(row)
+  # 'Whole Tag' is because attachments are currently in the wrong format
+  ['published', 'draft', 'submitted'].include?(row['State']) || row['Whole Tag'] == 'Closed'
+end
+
 input = CSV.parse($stdin, headers: true).select { |row|
   /#{Regexp.escape(ARGV.first)}/ =~ row['Old Url']
 }.collect { |row|
-  [row['Old Url'], row['New Url']] if ['published','draft','submitted'].include?(row['State'])
+  [row['Old Url'], row['New Url']] if include_row? row
 }
 
 out = CSV.generate do |csv|
