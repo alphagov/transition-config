0123b72b22c481e1e288d684eb54b1650974017a
psd <psd@whatfettle.com>
2013-01-31 16:03:26 +0000

    fixed wonky whitespace

--- run_redirect_regression_tests.sh
+++ run_redirect_regression_tests.sh
@@ -4,19 +4,19 @@ set -x
 
 echo DEPLOY_TO=$DEPLOY_TO
 
-prove -l tests/integration/config_rules/     \
-    	tests/integration/regression/   	 \
-	    tests/integration/sample/           \
-		tests/regression/businesslink_piplinks.t
+prove -l tests/integration/config_rules/ \
+	tests/integration/regression/ \
+	tests/integration/sample/ \
+	tests/regression/businesslink_piplinks.t
 
 (
 	IFS=,
-    read titles
-    while read site redirected rest
-    do
-    	if [ $redirected == Y ]; then
-            prove -l tests/unit/sources/${site}_valid_lines.t
+	read titles
+	while read site redirected rest
+	do
+		if [ $redirected == Y ]; then
+			prove -l tests/unit/sources/${site}_valid_lines.t
 			prove -l tests/regression/${site}.t
-        fi
-    done
+		fi
+	done
 ) < sites.csv
