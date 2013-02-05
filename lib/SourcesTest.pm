78a21bf9f8bb75138580af3f153de508090502d6
psd <psd@whatfettle.com>
2013-02-05 08:42:25 +0000

    validate_csv layout

--- lib/SourcesTest.pm
+++ lib/SourcesTest.pm
@@ -49,49 +49,25 @@ sub test_source_line {
     return if $mapping_status && $mapping_status !~ m{\bclosed\b};
 
     my $old_url = $row->{'Old Url'};
-    ok(
-        $old_url ne '#REF!',
-        "Old Url '${old_url}' should not be '#REF!'"
-    );
-    ok(
-        $old_url =~ m{^https?://},
-        "Old Url '${old_url}' should be a full URL"
-    );
 
-    # trim
-    $old_url =~ s{^\s*(.*?)\s*$}{$1};
+    ok($old_url ne '#REF!', "Old Url '${old_url}' should not be '#REF!'");
+    ok($old_url =~ m{^https?://}, "Old Url '${old_url}' should be a full URL");
 
     my $old_uri = URI->new($old_url);
-    is(
-        $old_uri,
-        $old_url,
-        "Old Url '${old_url}' should be a valid URL"
-    );
+    is($old_uri, $old_url, "Old Url '${old_url}' should be a valid URL");
 
     my $new_url = $row->{'New Url'};
-    ok(
-        $new_url !~ m{^http://www.gov.uk},
-        "'${old_url}' points to '${new_url}' - should point to HTTPS"
-    );
+    ok($new_url !~ m{^http://www.gov.uk}, "'${old_url}' points to '${new_url}' - should point to HTTPS");
 
     my $status = $row->{'Status'} // '';
-
-    #trim
-    $new_url =~ s{^\s*(.*?)\s*$}{$1};
 
     if ( "301" eq $status) {
         ok(($new_url ne ''), "missing new_url for 301");
 
         my $new_uri = URI->new($new_url);
 
-        ok(
-            $new_url =~ m{^https?://},
-            "${new_url} (from ${old_url}) should be a full URL"
-        );
-        is(
-            $new_uri,
-            $new_url,
-            "${new_url} (from ${old_url}) should be a valid URL"
+        ok($new_url =~ m{^https?://}, "${new_url} (from ${old_url}) should be a full URL");
+        is($new_uri, $new_url, "${new_url} (from ${old_url}) should be a valid URL"
         );
     } elsif ( "410" eq $status) {
         ok($new_url eq '', "unexpected New Url for 410: $new_url");
