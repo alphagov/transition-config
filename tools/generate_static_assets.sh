333207ef5cb24c7f31b493be736b552b8dc4fb76
psd <psd@whatfettle.com>
2013-02-05 15:29:23 +0000

    template static assets: 410, 404, robots.txt files

--- tools/generate_static_assets.sh
+++ tools/generate_static_assets.sh
@@ -4,24 +4,26 @@
 #  generate 410, 404 and other static assets for a site
 #
 
-set -e -x
+set -e
+
+# "$site" "$domain" "$redirection_date" "$tna_timestamp" "$title" "$new_site"
+
+site="$1"
+domain="$2"
+redirection_date="$3"
+tna_timestamp="$4"
+title="$5"
+new_site="$6"
+
+#
+#  ensure static directory exists
+#
+path=dist/static/$site/
+mkdir -p $path
 
 #
 #  generate 404 page
 #
-
-awk -F, '$1 == "'$1'" { print }' data/sites.csv | while read site domain redirection_dateTNA Timestamp,Title,New Site,Aliases
-do
-redirection_date=$2
-department_name=$3
-new_homepage=$4
-national_archives_timestamp=$5
-old_homepage=$6
-
-path=dist/static/$site/"
-
-mkdir -p $path
-
 cat > "${path}/404.html" <<EOF
 <!DOCTYPE html>
 <html class="no-branding">
@@ -36,13 +38,13 @@ cat > "${path}/404.html" <<EOF
         <header class="page-header group">
           <div class="legacy-site-logo"></div>
           <hgroup>
-            <h1>The website for the $department_name has been replaced by GOV.UK</h1>
+            <h1>The $title website title has been replaced by GOV.UK</h1>
           </hgroup>
         </header>
 
         <article role="article" class="group">
 
-          <p>On $redirection_date the website for the $department_name was replaced by <a href='http://www.gov.uk/$new_homepage'>gov.uk/$new_homepage</a>. <a href='https://www.gov.uk'>GOV.UK</a> is now the best place to find government services and information.</p>
+          <p>On $redirection_date the $title website was replaced by <a href='$new_site'>$new_homepage</a>. <a href='https://www.gov.uk'>GOV.UK</a> is now the best place to find government services and information.</p>
 
           <p>A copy of the page you were looking for may be found in <a href='http://webarchive.nationalarchives.gov.uk/'>The UK Government Web Archive</a>.</p>
 
@@ -53,10 +55,10 @@ cat > "${path}/404.html" <<EOF
 </html>
 EOF
 
-}
 
-local path="$(pwd)/redirector/static/$site/"
-mkdir -p $path
+#
+#  generate 410 page content
+#
 cat > "${path}/410.html" <<EOF
   <body>
     <section id="content" role="main" class="group">
@@ -70,13 +72,13 @@ cat > "${path}/410.html" <<EOF
 
         <article role="article" class="group">
 
-          <p>On $redirection_date the website for the $department_name was replaced by <a href='https://www.gov.uk/$new_homepage'>gov.uk/$new_homepage</a>.</p>
+          <p>On $redirection_date the $title website was replaced by <a href='$new_site'>gov.uk/$new_homepage</a>. <a href='https://www.gov.uk'>GOV.UK</a> is now the best place to find government services and information.</p>
 
-          <p>GOV.UK does not cover every piece of content that used to be found on the website for the $department_name. The page you are looking for is probably one of these.</p>
+          <p>GOV.UK does not cover every piece of content that used to be found on the $title website, and the page you are looking for is probably one of these.</p>
 
           <p>Essential government services and information can be found at <a href='https://www.gov.uk'>GOV.UK</a>.</p>
 
-          <p>A copy of the page you were looking for can be found in <a href="http://webarchive.nationalarchives.gov.uk/$national_archives_timestamp/$old_homepage<?= \$_SERVER['REQUEST_URI'] ?>">The UK Government Web Archive</a>, however it will not be updated after $redirection_date.</p>
+          <p>A copy of the page you were looking for can be found in <a href="http://webarchive.nationalarchives.gov.uk/$national_archives_timestamp/http://$domain<?= \$_SERVER['REQUEST_URI'] ?>">The UK Government Web Archive</a>, however it will not be updated after $redirection_date.</p>
 
 <?php include '410_suggested_links.php'; ?>
 
@@ -87,17 +89,29 @@ cat > "${path}/410.html" <<EOF
 </html>
 EOF
 
+#
+#  robots.txt
+#
+cat > $path/robots.txt <<EOF
+User-agent: *
+Disallow:
+Sitemap: http://$domain/sitemap.xml
+EOF
 
+#
+#  assemble 410 php file
+#
 touch dist/${domain}.suggested_links.conf
 touch dist/${domain}.archive_links.conf
+cp redirector/410_suggested_links.php $path
 
 cat \
     redirector/410_preamble.php \
     dist/${domain}.*suggested_links*.conf \
     dist/${domain}.archive_links.conf \
     redirector/410_header.php \
-    redirector/static/${site}/410.html \
-	> dist/static/${site}/410.php
+    $path/410.html \
+    > $path/410.php
 
-cp redirector/410_suggested_links.php dist/static/${site}
-done
+
+exit
