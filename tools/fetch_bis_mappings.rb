36c4f52a6a23294fc10a6a5b38be2c96e10ddff8
Chris Parsons <chris@thinkcodelearn.com>
2013-02-07 16:12:53 +0000

    Fix document_mappings.csv reference

--- tools/fetch_bis_mappings.rb
+++ tools/fetch_bis_mappings.rb
@@ -5,5 +5,5 @@ fetcher = MappingFetcher.new("bis")
 fetcher.add_source(
   MappingFetcher::RemoteCsvSource.new("https://docs.google.com/spreadsheet/pub?key=0AiP6zL-gKn64dEw3N1VFX1BWei1pMnkwcS1UYmVVbGc&output=csv"))
 
-fetcher.remap_new_urls_using(File.expand_path("../new_document_mappings.csv", File.dirname(__FILE__)))
+fetcher.remap_new_urls_using(File.expand_path("../document_mappings.csv", File.dirname(__FILE__)))
 fetcher.fetch
