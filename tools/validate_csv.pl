ac90da9b01dd624173540351ce8cfc2ca49844a0
psd <psd@whatfettle.com>
2013-02-07 16:47:23 +0000

    refactored tools/validate_csv.pl to be straight perl

--- tools/validate_csv.pl
+++ tools/validate_csv.pl
@@ -3,23 +3,6 @@
 #
 #  validate CSV file format
 #
-use Test::More;
-
-my $file = shift;
-my $domain = shift // "";
-my $whitelist = shift // "data/whitelist.txt";
-my $test = ValidateCSV->new($file, $domain);
-
-my $http_only = 1;
-
-$test->load_whitelist($whitelist);
-$test->run_tests();
-
-done_testing();
-exit;
-
-package ValidateCSV;
-
 use v5.10;
 use strict;
 use warnings;
@@ -30,39 +13,64 @@ use HTTP::Request;
 use LWP::UserAgent;
 use URI;
 
-sub new {
-    my $class = shift;
-    my $file  = shift;
-    my $domain  = shift;
+my $filename = shift;
+my $domain = shift // "";
+my $whitelist_filename = shift // "data/whitelist.txt";
+my $http_only = 1;
+my %whitelist = ();
 
-    my $self = {
-            input_file => $file,
-            domain => $domain,
-        };
+load_whitelist($whitelist_filename);
 
-    bless $self, $class;
-    return $self;
+test_file($filename);
+
+done_testing();
+
+exit;
+
+sub test_file {
+    my $filename = shift;
+    my $csv = Text::CSV->new({ binary => 1 }) or die "Cannot use CSV: " . Text::CSV->error_diag();
+
+    open(my $fh, "<", $filename) or die "$filename: $!";
+
+    my $names = $csv->getline($fh);
+    $csv->column_names(@$names);
+
+    while (my $row = $csv->getline_hr($fh)) {
+        test_row($row);
+    }
 }
 
-sub run_tests {
-    my $self = shift;
+sub test_row {
+    my $row  = shift;
 
-    my $csv = Text::CSV->new({ binary => 1 })
-                or die "Cannot use CSV: " . Text::CSV->error_diag();
+    my $old_url = $row->{'Old Url'} // '';
+    my $new_url = $row->{'New Url'} // '';
+    my $status = $row->{'Status'} // '';
 
-    open( my $fh, "<", $self->{'input_file'} )
-            or die "$self->{'input_file'}: $!";
+    my $old_uri = check_url('Old Url', $old_url);
 
-    my $names = $csv->getline( $fh );
-    $csv->column_names( @$names );
+    my $scheme = $old_uri->scheme;
 
-    while ( my $row = $csv->getline_hr( $fh ) ) {
-        $self->test($row);
+    is($scheme, 'http', "Old Url scheme [$scheme] must be [http] line $.") if ($http_only);
+
+    ok($old_url =~ m{^https?://$domain}, "Old Url [$old_url] domain not [$domain] line $.");
+
+    if ( "301" eq $status) {
+        my $new_uri = check_url('New Url', $new_url);
+        my $host = $new_uri->host;
+        ok($whitelist{$host}, "New Url [$new_url] host [$host] not whiltelist line $.");
+    } elsif ( "410" eq $status) {
+        ok($new_url eq '', "unexpected New Url [$new_url] for 410 line $.");
+    } elsif ( "200" eq $status) {
+        ok($new_url eq '', "unexpected New Url [$new_url] for 200 line $.");
+    } else {
+       fail("invalid Status [$status] for Old Url [$old_url] line $.");
     }
 }
 
 sub check_url {
-    my ($self, $name, $url) = @_;
+    my ($name, $url) = @_;
 
     $url =~ s/\|/%7C/g;
 
@@ -76,49 +84,13 @@ sub check_url {
     return $uri;
 }
 
-sub test_source_line {
-    my $self = shift;
-    my $row  = shift;
-    my $domain = $self->{domain};
-
-    my $old_url = $row->{'Old Url'} // '';
-    my $new_url = $row->{'New Url'} // '';
-    my $status = $row->{'Status'} // '';
-
-    my $old_uri = $self->check_url('Old Url', $old_url);
-
-    my $scheme = $old_uri->scheme;
-
-    is($scheme, 'http', "Old Url scheme [$scheme] must be [http] line $.") if ($http_only);
-
-    ok($old_url =~ m{^https?://$domain}, "Old Url [$old_url] domain not [$domain] line $.");
-
-    if ( "301" eq $status) {
-        my $new_uri = $self->check_url('New Url', $new_url);
-        my $host = $new_uri->host;
-        ok($self->{whitelist}->{$host}, "New Url [$new_url] host [$host] not whiltelist line $.");
-    } elsif ( "410" eq $status) {
-        ok($new_url eq '', "unexpected New Url [$new_url] for 410 line $.");
-    } elsif ( "200" eq $status) {
-        ok($new_url eq '', "unexpected New Url [$new_url] for 200 line $.");
-    } else {
-       fail("invalid Status [$status] for Old Url [$old_url] line $.");
-    }
-}
-
-sub test {
-    my $self = shift;
-    $self->test_source_line(@_);
-}
-
 sub load_whitelist {
-    my $self = shift;
     my $filename = shift;
     local *FILE;
     open(FILE, "< $filename") or die "unable to open whitelist $filename";
     while (<FILE>) {
         chomp;
         $_ =~ s/\s*\#.*$//;
-        $self->{whitelist}->{$_} = 1 if ($_);
+        $whitelist{$_} = 1 if ($_);
     }
 }
