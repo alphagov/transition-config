daf5f38496e8947243e1ce91ead0141232d47b57
psd <psd@whatfettle.com>
2013-02-08 12:26:26 +0000

    dedupe refactored to be in-memory

--- tools/dedupe.pl
+++ tools/dedupe.pl
@@ -7,45 +7,58 @@ use v5.10;
 use strict;
 use warnings;
 
+my $titles;
 my %seen = ();
 
     while (<STDIN>) {
         chomp;
+
+        unless ($titles) {
+            $titles = $_;
+            next;
+        }
+
         my ($old, $new, $status) = split(/,/);
 
-	# c14n url
-	my $url = $old;
+        # c14n url
+        my $url = $old;
         $url =~ s/\?*$//;
         $url =~ s/\/*$//;
         $url =~ s/\#*$//;
 
-	my $line = $_;
-	$line =~ s/^[^,]*,//;
-	$line = "$url,$line";
+        # line to be printed
+        my $line = $_;
+        $line =~ s/^[^,]*,//;
+        $line = "$url,$line";
 
-	$url = lc($url);
+        # case-insensitive matchin
+        $url = lc($url);
 
         if ($seen{$url}) {
-		if ($new eq $seen{$url}->{new} && $status eq $seen{$url}->{status}) {
-			say STDERR "ditching $url line $.";
-			next;
-		} else {
-			if ($status eq $seen{$url}->{status}) {
-				say STDERR "leaving duplicate $url [new url differs] line $.";
-			} else {
-				say STDERR "leaving duplicate $url [status differs] line $.";
-			}
-			say STDERR "> " . $line;
-			say STDERR "> " . $seen{$url}->{line};
-			say STDERR "";
-		}
+            if ($new eq $seen{$url}->{new} && $status eq $seen{$url}->{status}) {
+                say STDERR "ditching $url line $.";
+                next;
+            } else {
+                if ($status eq $seen{$url}->{status}) {
+                    say STDERR "leaving duplicate $url [new url differs] line $.";
+                } else {
+                    say STDERR "leaving duplicate $url [status differs] line $.";
+                }
+                say STDERR "> " . $line;
+                say STDERR "> " . $seen{$url}->{line};
+                say STDERR "";
+            }
         }
 
-	say "$line";
-
         $seen{$url} = {
-		'new' => $new,
-		'status' => $status,
-		'line' => $line,
+            'new' => $new,
+            'status' => $status,
+            'line' => $line,
         };
     }
+
+    say $titles;
+    foreach my $url (sort keys %seen) {
+         say $seen{$url}->{line};
+    }
+
