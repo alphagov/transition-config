81faaff2d177db141ca7a844d06e196a7c676501
psd <psd@whatfettle.com>
2013-02-12 10:52:24 +0000

    test for validate_mappings

--- tests/tools/tidy_mappings.sh
+++ tests/tools/tidy_mappings.sh
@@ -16,7 +16,7 @@ http://example.com/4,http://snark.com,301
 http://example.com/4,http://snork.com,301
 !
 
-cmp /tmp/tidy.err - <<!
+diff /tmp/tidy.err - <<!
 skipping http://example.com/1 line 4
 replacing 410 http://example.com/2 with 301 line 6
 skipping 410 http://example.com/3 for duplicate 301 line 8
@@ -26,9 +26,9 @@ leaving 301 http://example.com/4 duplicates differ line 10
 
 !
 
-[ $? -ne 0 ] && { cat /tmp/tidy.err ; echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
 
-cmp /tmp/tidy.out - <<!
+diff /tmp/tidy.out - <<!
 Old Url,New Url,Status,Stuff
 http://example.com/1,http://foo/1,301
 http://example.com/2,http://foo/2,301
@@ -57,7 +57,7 @@ http://www.businesslink.gov.uk,https://www.gov.uk,301,junk,wonk
 http://www.businesslink.gov.uk,http://example.com,301,wink,wink
 !
 
-cmp /tmp/tidy-a.err - <<!
+diff /tmp/tidy-a.err - <<!
 skipping http://example.com/1 line 3
 replacing 410 http://example.com/2 with 301 line 5
 skipping 410 http://example.com/3 for duplicate 301 line 7
@@ -65,9 +65,9 @@ using actual http://www.direct.gov.uk,https://www.gov.uk,301,foo,bar,baz line 9
 using actual http://www.businesslink.gov.uk,https://www.gov.uk,301,wink,wink line 11
 !
 
-[ $? -ne 0 ] && { cat /tmp/tidy-a.err ; echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
 
-cmp /tmp/tidy-a.out - <<!
+diff /tmp/tidy-a.out - <<!
 Old Url,New Url,Status,More,Stuff
 http://example.com/1,http://foo/1,301
 http://example.com/2,http://foo/2,301
@@ -89,12 +89,12 @@ http://example.com/2,,410
 http://example.com/3,,410
 !
 
-cmp /tmp/tidy-t.err - <<!
+diff /tmp/tidy-t.err - <<!
 !
 
-[ $? -ne 0 ] && { cat /tmp/tidy-a.err ; echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
 
-cmp /tmp/tidy-t.out - <<!
+diff /tmp/tidy-t.out - <<!
 Old Url,New Url,Status,More,Stuff
 http://example.com/1,http://foo/3,301
 http://example.com/2,,410
