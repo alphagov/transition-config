075e874beec962285e715ff9ae6a6de5fdb2b625
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-02-13 16:20:43 +0000

    Committing the tests used for propagating 418s
    These are not fully formed tests but I don't think this is worth the time given that the whole munge process is about to be refactored. However, they were useful and took time to put together so committing, for probably later deletion...

---
+++ munge/test_adding_418s.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby -w
+
+# A test script to check the 418 propagation
+# Committing as is. Can be made into a proper test if required, or removed after completion of story separating munge into pipeline
+# Usage:
+# ./munge/extract-mappings.rb www.test.gov.uk < ./munge/tests/test_mappings.csv | munge/test_adding_418s.rb
+# Creates a data/mappings/test.csv
+
+base_dir = File.expand_path("..", File.dirname(__FILE__))
+
+require_relative "mapping_fetcher"
+fetcher = MappingFetcher.new("test")
+
+fetcher.add_source(MappingFetcher::StringCsvSource.new($stdin))
+fetcher.remap_new_urls_using(base_dir + "/document_mappings.csv")
+fetcher.fetch
