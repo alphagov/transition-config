8e82df8914786bf5b05d3acde7ffc664c7664e9c
psd <psd@whatfettle.com>
2013-02-14 09:37:42 +0000

    fixed site_hosts.sh tests

--- tests/tools/site_hosts.sh
+++ tests/tools/site_hosts.sh
@@ -4,14 +4,15 @@
 #  simplest
 #
 #  Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options
-cat > /tmp/sites-hosts-test.csv  <<-!
+test=/tmp/sites-hosts-test.csv
+cat > $test <<-!
 Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options
 site,one.example.com,Redirection Date,TNA Timestamp,Title,New Site,alias1.example.com alias2.example.com,Validate Options
 site,two.example.com,Redirection Date,TNA Timestamp,Title,New Site,alias3.example.com,Validate Options
 site,three.example.com,Redirection Date,TNA Timestamp,Title,New Site,alias4.example.com,Validate Options
 !
 
-tools/site_hosts.sh --sites /tmp/sites-test.csv > /tmp/site-hosts.out 2> /tmp/site-hosts.err <<!
+tools/site_hosts.sh --sites $test > /tmp/site-hosts.out 2> /tmp/site-hosts.err <<!
 
 diff /tmp/site-hosts.err - <<!
 !
