d5db0f6f4eb736cdd09997d65010edc698ea5f33
psd <psd@whatfettle.com>
2013-02-14 09:24:47 +0000

    test_static_assets to check sitemap.xml, robots.txt for each site

---
+++ tools/test_static_assets.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+#  test static assets served for each host mentioned in sites.csv
+#
+cmd=$(basename $0)
+sites="data/sites.csv"
+
+usage() {
+    echo "usage: $cmd) [opts] [-- test_mappings opts]" >&2
+    echo "    [-s|--sites sites.csv]      sites file" >&2
+    echo "    [-?|--help]                 print usage" >&2
+    exit 1
+}
+
+while test $# -gt 0 ; do
+    case "$1" in
+    -s|--sites) shift; sites="$1" ; shift ; continue;;
+    -\?|-h|--help) usage ;;
+    --) break ;;
+    -*) usage ;;
+    esac
+    break
+done
+
+mappings=$(
+cut -d, -f2 "$sites" |
+    while read host
+    do
+        echo "http://$host/facicon.ico,,200"
+        echo "http://$host/robots.txt,,200"
+        echo "http://$host/sitemap.xml,,200"
+        echo "http://$host/404.php,,404"
+        echo "http://$host/410.php,,410"
+    done)
+
+prove tools/test_mappings.pl :: -m "$@" $mappings
+
+exit 0
