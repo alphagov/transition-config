d023a449a2d090abb90449f580f8acff4fa2f6a1
Chris Parsons <chris@thinkcodelearn.com>
2013-02-15 16:09:17 +0000

    Remove [[ (not supported)

--- tests/tools/sites_csv_order.sh
+++ tests/tools/sites_csv_order.sh
@@ -6,6 +6,6 @@
 
 header=`head -n1 data/sites.csv`
 
-[[ "$header" == 'Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options' ]] || { echo "$0: FAIL: sites.csv has had its columns changed. Please update this test and generate-redirects.sh's use of sites.csv." ; exit 2; }
+[ "$header" == 'Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options' ] || { echo "$0: FAIL: sites.csv has had its columns changed. Please update this test and generate-redirects.sh's use of sites.csv." ; exit 2; }
 
 echo "$0: OK"
