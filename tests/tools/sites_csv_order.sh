0aa42bc2ca2bc214cf69fb35e0d7e6d2b086fb46
Chris Parsons <chris@thinkcodelearn.com>
2013-02-15 16:11:18 +0000

    Use bash not sh

--- tests/tools/sites_csv_order.sh
+++ tests/tools/sites_csv_order.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Must ensure that the csv columns remain in the same order,
 # as ./munge/generate-redirects.sh relies on them. When this
@@ -6,6 +6,6 @@
 
 header=`head -n1 data/sites.csv`
 
-[ "$header" == 'Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options' ] || { echo "$0: FAIL: sites.csv has had its columns changed. Please update this test and generate-redirects.sh's use of sites.csv." ; exit 2; }
+[[ "$header" == 'Site,Host,Redirection Date,TNA Timestamp,Title,New Site,Aliases,Validate Options' ]] || { echo "$0: FAIL: sites.csv has had its columns changed. Please update this test and generate-redirects.sh's use of sites.csv." ; exit 2; }
 
 echo "$0: OK"
