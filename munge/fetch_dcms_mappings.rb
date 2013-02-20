9f15ce719091e71419f765545f07c5ef1ccd8a8b
Chris Parsons <chris@thinkcodelearn.com>
2013-02-20 10:35:34 +0000

    New munge script for DCMS

---
+++ munge/fetch_dcms_mappings.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -w
+
+require 'csv'
+require_relative 'mapping_fetcher'
+
+fetcher = MappingFetcher.new
+
+fetcher.add_source(StringCsvSource.new($stdin))
+# Note if there are duplicates then the FIRST mapping is used, so order matters here
+{
+  'Harvester Data' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=3',
+  'Friendly URLS' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=6',
+  'Manual Mappings' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=0',
+  'Sitemap' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=4',
+  'Analytics' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=8',
+  'Any other URLs' => 'https://docs.google.com/spreadsheet/pub?key=0AlVEZKtKyUEvdGpUckZ3OThKVUk5U1M5VFI3ZHc2cmc&output=csv&gid=10'
+}.each do |_, url|
+  fetcher.add_source(RemoteCsvSource.new(url))
+end
+
+fetcher.fetch
