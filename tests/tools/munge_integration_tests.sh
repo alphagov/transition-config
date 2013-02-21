aab55314e6250a7d899a631c1468c8c201da8cdf
Chris Parsons <chris@thinkcodelearn.com>
2013-02-21 09:00:57 +0000

    Use ENV to set collation sequence

--- tests/tools/munge_integration_tests.sh
+++ tests/tools/munge_integration_tests.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # tests that admin urls are mapped to public
 # and that urls which appear first trump older urls
+
+. ./tools/env
 
 test_document_mappings='/tmp/munge_test_document_mappings.csv'
 fetched_data='/tmp/munge_test_fetched_data.csv'
@@ -24,8 +26,8 @@ Old Url,New Url,Status,Whole Tag,Slug,Admin Url,State
 
 cat > $fetched_data <<!
 old url,new url,status,source,row_number
-http://www.decc.gov.uk/bar,https://whitehall-admin.production.alphagov.co.uk/government/admin/policy_advisory_groups/88/edit,,passed in string,1
-http://www.decc.gov.uk/bar,https://gov.uk/this-should-be-trumped-and-not-appear,,passed in string,2
+http://www.decc.gov.uk,https://whitehall-admin.production.alphagov.co.uk/government/admin/policy_advisory_groups/88/edit,,passed in string,1
+http://www.decc.gov.uk,https://gov.uk/this-should-be-trumped-and-not-appear,,passed in string,2
 http://www.decc.gov.uk/foo,https://gov.uk/this-is-foo,,passed in string,3
 http://www.decc.gov.uk/foo?,https://gov.uk/this-should-not-appear-as-foo,,passed in string,3
 http://www.decc.gov.uk/foo#,https://gov.uk/this-should-not-appear-as-foo,,passed in string,3
@@ -35,11 +37,11 @@ run_munge
 
 diff $output - <<!
 Old Url,New Url,Status
-http://www.decc.gov.uk/bar,https://www.gov.uk/government/policies/remapped-public-url,301
+http://www.decc.gov.uk,https://www.gov.uk/government/policies/remapped-public-url,301
 http://www.decc.gov.uk/foo,https://gov.uk/this-is-foo,301
 !
 
-[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { error "$0: FAIL" ; exit 1; }
 
 # test: ensure that different cases are treated correctly with trumping
 
@@ -56,7 +58,7 @@ Old Url,New Url,Status
 http://www.decc.gov.uk/foo,https://gov.uk/this-is-foo,301
 !
 
-[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { error "$0: FAIL" ; exit 1; }
 
 # test: ensure that we fold mappings correctly
 
@@ -76,7 +78,7 @@ http://www.decc.gov.uk/foo,https://gov.uk/this-is-the-end-target,301
 http://www.decc.gov.uk/quux,https://gov.uk/this-is-the-end-target,301
 !
 
-[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
+[ $? -ne 0 ] && { error "$0: FAIL" ; exit 1; }
 
 # test: ensure we capture the status correctly when it changes
 
@@ -94,6 +96,4 @@ http://www.decc.gov.uk/bar,,410
 http://www.decc.gov.uk/foo,,410
 !
 
-[ $? -ne 0 ] && { echo "$0: FAIL" ; exit 1; }
-
-echo "$0: OK"
+[ $? -ne 0 ] && { error "$0: FAIL" ; exit 1; }
