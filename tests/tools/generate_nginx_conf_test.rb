a9fca78fbe288619c113142d3bfba460b2b2a67e
psd <psd@whatfettle.com>
2013-03-11 15:54:00 +0000

    check innards

--- tests/tools/generate_nginx_conf_test.rb
+++ tests/tools/generate_nginx_conf_test.rb
@@ -5,26 +5,18 @@ require 'open3'
 class GenerateNginxConfTest < MiniTest::Unit::TestCase
   def test_can_generate_nginx_vhost_given_hostname
     stdout, stderr, status = Open3.capture3(%{tools/generate_nginx_conf.sh --homepage "http://www.bar.com" --site foo www.foo.com})
-    expected_generated_config = %q{server {
-    server_name     www.foo.com
-                    aka.foo.com;
-
-    root            /var/apps/redirector/static/foo;
-    include         /var/apps/redirector/common/settings.conf;
-    include         /var/apps/redirector/common/status_pages.conf;
-    include         /var/apps/redirector/maps/foo/location.conf;
-
-    location = /    { return 301 http://www.bar.com; }
-}
-}
 
     assert_equal 0, status.exitstatus
-    assert_equal expected_generated_config, stdout
-    assert_match %r{server\s+\{\s+server_name\s+www.foo.com\s+aka.foo.com;}, stdout
-    assert_match %r{root\s+/var/apps/redirector/static/foo;}, stdout
-    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, stdout
-    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, stdout
-    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, stdout
-    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, stdout
+
+    match = %r{\bserver\s+\{(.*)\}}m.match(stdout)
+    assert match
+    innards = match[1]
+
+    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com;}, innards
+    assert_match %r{root\s+/var/apps/redirector/static/foo;}, innards
+    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, innards
+    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, innards
+    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, innards
+    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, innards
   end
 end
