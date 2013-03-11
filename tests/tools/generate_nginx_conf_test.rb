f556d54199a0dbd8ad14a470e3ba6a8ef3af7529
psd <psd@whatfettle.com>
2013-03-11 16:07:31 +0000

    check innards

--- tests/tools/generate_nginx_conf_test.rb
+++ tests/tools/generate_nginx_conf_test.rb
@@ -4,19 +4,36 @@ require 'open3'
 
 class GenerateNginxConfTest < MiniTest::Unit::TestCase
   def test_can_generate_nginx_vhost_given_hostname
-    stdout, stderr, status = Open3.capture3(%{tools/generate_nginx_conf.sh --homepage "http://www.bar.com" --site foo www.foo.com})
+    server_declaration_contents = generate_nginx_config('http://www.bar.com', 'foo', 'www.foo.com')
+
+    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com;}, server_declaration_contents
+    assert_match %r{root\s+/var/apps/redirector/static/foo;}, server_declaration_contents
+    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, server_declaration_contents
+    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, server_declaration_contents
+    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, server_declaration_contents
+    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, server_declaration_contents
+  end
+
+  def test_aka_prefix_created_for_hostname_with_www_prefix
+  end
+
+  def test_can_generate_nginx_vhost_multiple_aliases
+    stdout, stderr, status = Open3.capture3(%{ tools/generate_nginx_conf.sh --homepage http://www.snork.com/foo --site foo www.foo.com www.bar.com bar.foo.com})
 
     assert_equal 0, status.exitstatus
+    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com
+    
+    ;}, server_declaration_contents
+  end
 
-    match = %r{\bserver\s+\{(.*)\}}m.match(stdout)
-    assert match
-    innards = match[1]
+  def parse_server_declaration(output)
+    %r{\bserver\s+\{(.*)\}}m.match(output)[1]
+  end
 
-    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com;}, innards
-    assert_match %r{root\s+/var/apps/redirector/static/foo;}, innards
-    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, innards
-    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, innards
-    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, innards
-    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, innards
+  def generate_nginx_config(homepage, site, *aliases)
+    cmd = "tools/generate_nginx_conf.sh --homepage #{homepage} --site #{site} #{aliases.join(' ')}"
+    stdout, stderr, status = Open3.capture3(cmd)
+    assert_equal 0, status.exitstatus
+    parse_server_declaration(stdout)
   end
 end
