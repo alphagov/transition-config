4dc75ba4ff72426bb4ba0c59f9fd601a94b5939b
psd <psd@whatfettle.com>
2013-03-22 12:59:44 +0000

    smoke_tests working with all data/tests for now

--- tools/smoke_tests.sh
+++ tools/smoke_tests.sh
@@ -6,13 +6,13 @@ set -e
 
 status DEPLOY_TO=$DEPLOY_TO
 
-dir="data/subsets"
+testdir=data/tests
 
 status "Checking test coverage ..."
-tools/test_coverage.sh --name $dir --sites data/sites.csv $dir/*
+tools/test_coverage.sh --name "test data $testdir" --sites data/sites.csv $testdir/*
 
 status "Testing static assets ..."
 tools/test_static_assets.sh --sites data/sites.csv
 
-status "Testing CSV files from $dir ..."
-prove -l tools/test_mappings.pl :: $@ $dir/*.csv
+status "Testing CSV files from $testdir ..."
+prove -l tools/test_mappings.pl :: $@ $testdir/*.csv
