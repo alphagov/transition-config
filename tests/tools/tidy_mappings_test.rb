ac1543947fe7c08ecaf7277456c881224a3db077
psd <psd@whatfettle.com>
2013-03-22 12:56:23 +0000

    moved tidy mappings tests to ruby

--- tests/tools/tidy_mappings_test.rb
+++ tests/tools/tidy_mappings_test.rb
@@ -4,185 +4,168 @@ require 'open3'
 
 class TidyMappingsTest < MiniTest::Unit::TestCase
   def test_default_410_status_for_a_status_of_TNA
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,,TNA"
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,,410", lines[1]
   end
 
   def test_default_410_status_for_a_missing_new_url
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,,",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,,410", lines[1]
   end
 
   def test_blank_new_url_for_a_410
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://foo.com,410",
       "http://example.com/2,http://foo.com,TNA",
       ])
-    lines = stdout.split("\n")
     assert_equal 3, lines.size
     assert_equal "http://example.com/1,,410", lines[1]
     assert_equal "http://example.com/2,,410", lines[2]
   end
 
   def test_default_301_status_with_a_new_url
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://foo/1,",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_identical_mappings_are_skipped
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://foo/1,301",
       "http://example.com/1,http://foo/1,301"
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_301_replaces_a_410_with_the_same_old_url
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,,410",
       "http://example.com/1,http://foo/1,301",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_410_is_skipped_where_a_301_exists_with_the_same_old_url
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://foo/1,301",
       "http://example.com/1,,410",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_duplicate_301s_with_the_same_old_url_but_different_new_urls_are_skipped
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://snark.com,301",
       "http://example.com/1,http://snork.com,301",
       ])
-    lines = stdout.split("\n")
     assert_equal 3, lines.size
     assert_equal "http://example.com/1,http://snark.com,301", lines[1]
     assert_equal "http://example.com/1,http://snork.com,301", lines[2]
   end
 
   def test_duplicate_301s_with_the_same_old_url_but_different_new_urls_are_skipped
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://snark.com,301",
       "http://example.com/1,http://snork.com,301",
       ])
-    lines = stdout.split("\n")
     assert_equal 3, lines.size
     assert_equal "http://example.com/1,http://snark.com,301", lines[1]
     assert_equal "http://example.com/1,http://snork.com,301", lines[2]
   end
 
   def test_blacklisted_paths_may_not_be_redirected
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/,http://snark.com,301",
       "http://example.com/1,http://foo/1,301",
       "http://example.com/robots.txt,http://foo/robots,301",
       ], blacklist: fixture_file("blacklist.txt"))
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_additional_csv_fields_retained_in_output
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://snark.com,301,extra,stuff",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://snark.com,301,extra,stuff", lines[1]
   end
 
   def test_trump_causes_later_mappings_to_win
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://foo/1,301",
       "http://example.com/1,,410"
       ], trump: true)
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,,410", lines[1]
   end
 
   def test_query_strings_are_removed_from_old_url_by_default
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1?foo=bar,http://foo/1,301",
       ])
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,http://foo/1,301", lines[1]
   end
 
   def test_query_string_option_retains_query_strings_in_old_urls
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1?foo=bar,http://foo/1,301",
       ], query_string: '-')
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1?foo=bar,http://foo/1,301", lines[1]
   end
 
   def test_query_string_option_whitelists_parameters_in_old_urls
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1?foo=bar&token=99&snark=snork&id=34,http://foo/1,301",
       ], query_string: 'id:token')
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1?id=34&token=99,http://foo/1,301", lines[1]
   end
 
   def test_new_urls_which_are_furls_are_expanded_using_sites
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,https://www.gov.uk/dotnet,301"
       ], sites: fixture_file("sites.csv"))
-    lines = stdout.split("\n")
     assert_equal 2, lines.size
     assert_equal "http://example.com/1,https://new.example.net/example/net/long/url,301", lines[1]
   end
 
   def test_new_urls_which_are_a_known_host_homepage_are_expanded_using_sites
-    stdout = tidy_mappings([
+    lines = tidy_mappings([
       "Old Url,New Url,Status,Stuff",
       "http://example.com/1,http://www.example.com/,301",
       "http://example.com/2,http://www.example.net/,301",
       "http://example.com/3,http://ano.example.net/,301",
       ], sites: fixture_file("sites.csv"))
-    lines = stdout.split("\n")
     assert_equal 4, lines.size
     assert_equal "http://example.com/1,https://new.example.com/example/com/long/url,301", lines[1]
     assert_equal "http://example.com/2,https://new.example.net/example/net/long/url,301", lines[2]
@@ -213,7 +196,7 @@ class TidyMappingsTest < MiniTest::Unit::TestCase
       puts stderr
     end
     assert_equal 0, status.exitstatus
-    stdout
+    stdout.split("\n")
   end
 
   def fixture_file(filename)
