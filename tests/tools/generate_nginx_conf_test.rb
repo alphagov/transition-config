b3c43e545f85ab6345d39c4575f11660d5e217a9
psd <psd@whatfettle.com>
2013-03-24 15:41:30 +0000

    better ruby nginx.conf tests

--- tests/tools/generate_nginx_conf_test.rb
+++ tests/tools/generate_nginx_conf_test.rb
@@ -1,38 +1,55 @@
 require 'minitest/unit'
 require 'minitest/autorun'
+require 'tempfile'
 require 'open3'
 
 class GenerateNginxConfTest < MiniTest::Unit::TestCase
   def test_can_generate_nginx_vhost_given_hostname
-    server_declaration_contents = generate_nginx_config('http://www.bar.com', 'foo', 'www.foo.com')
+    server_declaration = generate_nginx_config([
+        'site: foo',
+        'host: www.example.com',
+        'redirection_date: 13th December 2012',
+        'tna_timestamp: 20120816224015',
+        'title: Example&#39;s Office',
+        'furl: www.gov.uk/foo',
+        'homepage: http://www.bar.com'
+        ])
 
-    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com;}, server_declaration_contents
-    assert_match %r{root\s+/var/apps/redirector/static/foo;}, server_declaration_contents
-    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, server_declaration_contents
-    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, server_declaration_contents
-    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, server_declaration_contents
-    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, server_declaration_contents
+    assert_match %r{server_name\s+www.example.com\s+aka.example.com\s*;}, server_declaration
+    assert_match %r{root\s+/var/apps/redirector/static/foo;}, server_declaration
+    assert_match %r{include\s+/var/apps/redirector/common/settings.conf;}, server_declaration
+    assert_match %r{include\s+/var/apps/redirector/common/status_pages.conf;}, server_declaration
+    assert_match %r{include\s+/var/apps/redirector/maps/foo/location.conf;}, server_declaration
+    assert_match %r{location = /\s*{ return 301 http://www.bar.com; }}, server_declaration
   end
 
-  def test_can_generate_nginx_vhost_aka_generated_for_each_www
-    server_declaration_contents = generate_nginx_config('http://www.bar.com', 'foo', 'www.foo.com', 'www.bar.com')
+  def test_multiple_aliases
+    server_declaration = generate_nginx_config([
+        'site: foo',
+        'host: www.example.com',
+        'homepage: http://www.bar.com',
+        'aliases:',
+        '  - www.foo.com',
+        '  - bar.com'
+        ])
 
-    assert_match %r{server_name\s+www.foo.com\s+aka.foo.com\s+www.bar.com\s+aka.bar.com;}, server_declaration_contents
+    assert_match %r{server_name\s+www.example.com\s+aka.example.com\s+www.foo.com\s+aka.foo.com\s+bar.com\s+aka-bar.com\s*;}, server_declaration
   end
 
-  def test_can_generate_nginx_vhost_aka_generated_for_non_www_host
-    server_declaration_contents = generate_nginx_config('http://www.bar.com', 'foo', 'foo.com')
-
-    assert_match %r{server_name\s+foo.com\s+aka-foo.com;}, server_declaration_contents
-  end
 
   def parse_server_declaration(output)
     %r{\bserver\s+\{(.*)\}}m.match(output)[1]
   end
 
-  def generate_nginx_config(homepage, site, *aliases)
-    cmd = "tools/generate_nginx_conf.sh --homepage #{homepage} --site #{site} #{aliases.join(' ')}"
+  def generate_nginx_config(stdin)
+    tmp = Tempfile.new('nginx_yml')
+    tmp << "---\n"
+    tmp << [*stdin].join("\n")
+    tmp << "\n---\n"
+    tmp.close
+    cmd = "bin/erb.rb -y #{tmp.path} templates/nginx.erb"
     stdout, stderr, status = Open3.capture3(cmd)
+    puts stderr if (stderr)
     assert_equal 0, status.exitstatus
     parse_server_declaration(stdout)
   end
