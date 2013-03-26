59790b2403ec3e566ae51d3de1fdcf883c32c194
psd <psd@whatfettle.com>
2013-03-26 15:14:24 +0000

    ditched tracing from dns_report

--- tools/dns_report.sh
+++ tools/dns_report.sh
@@ -6,9 +6,7 @@ mkdir -p $tmpdir
 tools/site_hosts.sh |
     while read host
     do
-        set -x
         dig +trace $host > $tmpdir/$host.txt
-        set +x
     done
 
 cat $tmpdir/*.txt | tools/dns_report.pl
