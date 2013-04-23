81dd8cbca32f916dde0588e574f5eb4cd7de570b
psd <psd@whatfettle.com>
2013-04-23 17:55:20 +0100

    use named DNS server for report

--- tools/dns_report.sh
+++ tools/dns_report.sh
@@ -2,11 +2,12 @@
 
 tmpdir=tmp/dns_report
 mkdir -p $tmpdir
+[ -z "$DNS_SERVER" ] && DNS_SERVER=8.8.8.8
 
 tools/site_hosts.sh |
     while read host
     do
-        dig +trace $host > $tmpdir/$host.txt
+        dig @$DNS_SERVER +trace $host > $tmpdir/$host.txt
     done
 
 cat $tmpdir/*.txt | tools/dns_report.pl $tmpdir/hosts.csv
