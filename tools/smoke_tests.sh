b2ef9a358b304a2e881c16939341687109e1b027
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-08-29 12:51:09 +0000

    Reorder smoke tests to gather more information
    test_static_assets can fail fast, but especially for reaching feature parity in
    bouncer, it's more useful to see the output of test_mappings before the failing
    tests in test_static_assets.

--- tools/smoke_tests.sh
+++ tools/smoke_tests.sh
@@ -11,8 +11,8 @@ testdir=data/tests
 echo "Checking test coverage ..."
 tools/test_coverage.sh $testdir/*.csv
 
-echo "Testing static assets ..."
-tools/test_static_assets.sh
-
 echo "Testing CSV files from $testdir ..."
 tools/test_mappings.pl $@ $testdir/*.csv
+
+echo "Testing static assets ..."
+tools/test_static_assets.sh
