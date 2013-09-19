153ba5cab4c158f98a66d6f6e8d9fa2ee236a438
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2013-09-19 19:11:18 +0100

    Update allsites munge to ignore on ignore_sites

--- munge/generate-redirects-allsites.sh
+++ munge/generate-redirects-allsites.sh
@@ -4,20 +4,23 @@ rm -f cache/whitehall.csv &&
 rm -rf backup &&
 mkdir -p cache/backup/ && 
 
-for SITE in `ls data/sites/directgov* | sed 's/\.yml//g'`
+for SITE in `ls data/sites | sed 's/\.yml//g'`
 do
-	if [ -z "$SITE" ]
+	IGNORE=`grep -x $SITE data/ignore_sites.txt`
+	if [ -z $SITE ]
 	then
-		echo "Blank line in $all_sites"
-	elif [ -z `grep $SITE data/ignore_sites.txt` ]; then
-        echo "Ignored $SITE"
-        
+		echo "Blank line in checklist"
+	elif [ "$SITE" = "$IGNORE" ]
+	then
+		echo "Excluded - ignoring: $SITE"
 	else
+		echo "Included - munging: $SITE"
 		mkdir -p cache/$SITE
 		sh ./munge/generate-redirects.sh -s data/sites -u betademo:nottobes -w data/whitelist.txt $SITE
   		cp data/mappings/$SITE.csv cache/backup/$SITE.csv &&
         mkdir -p cache/backup/$SITE
 		mv cache/$SITE/*.csv cache/backup/$SITE
+		
 	fi
 done &&
 
