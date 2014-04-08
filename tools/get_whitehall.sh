811cfc330e8bc8fb4c4910e9c700a78ade7ee0b5
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-04-08 11:06:51 +0100

    Add tool to get copies of the whitehall mappings files quickly

---
+++ tools/get_whitehall.sh
@@ -0,0 +1,3 @@
+
+curl -u betademo:nottobes https://whitehall-admin.production.alphagov.co.uk/government/mappings.csv > _whitehall_mappings.csv
+curl -u betademo:nottobes https://whitehall-admin.production.alphagov.co.uk/government/all_document_attachment_and_non_document_mappings.csv > _whitehall_all_mappings.csv
