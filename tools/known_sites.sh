154aa4a660a1f107a2b130e6ffac390badc807e3
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-05-02 12:05:00 +0100

    Exclude www.gov.uk from known_domains tool

--- tools/known_sites.sh
+++ tools/known_sites.sh
@@ -5,6 +5,9 @@ known_domains='known_domains.txt'
 
 #Establish all the domains we know about from configuration
 
-cat data/sites/*$sitefocus* data/transition-sites/*$sitefocus* | grep -o -i -E '[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]{1,61}\.\w{2,6}' | sort | uniq
+cat data/sites/*$sitefocus* data/transition-sites/*$sitefocus* |
+    grep -o -i -E '[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]*\.?[a-zA-Z0-9-]{1,61}\.\w{2,6}' |
+    grep -v 'www.gov.uk' |
+    sort | uniq
 
 
