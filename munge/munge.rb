b2bf2fa51e7c1978285ffd02fa79fdbfcbe56c1a
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-09-11 13:15:11 +0000

    Delete obvious redirector app files

--- munge/munge.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative "mapping_fetcher"
-
-fetcher = MappingFetcher.new
-fetcher.remap_new_urls_using(ARGV[0])
-puts fetcher.munge(StringCsvSource.new($stdin).input_csv)
--- munge/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e -x
-rake -f munge/Rakefile test
-
-for t in tests/munge/*.sh ; do set -x ; $t ; set +x ; done
-
-exit 0
