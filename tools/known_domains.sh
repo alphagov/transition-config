fe4b4a31e92814e50b4175120a61b3c49b6950f3
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-10-06 11:11:49 +0100

    Add commentary arount tool usage

--- tools/known_domains.sh
+++ tools/known_domains.sh
@@ -1,3 +1,10 @@
+# Usage:
+#
+#   from redirector root directory
+#   tools/known_domains.sh
+#
+# Outputs list of all hosts and aliases to to a textfile in redirector/cache
+
 mkdir cache
 touch cache/known_domains
 cat data/transition-sites/* | sed 's/^- /host: /' | grep host | awk '{ print $2 }' | sort | uniq > cache/known_domains
